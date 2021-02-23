c1e36e8a4aa1e9d1bc63e14bc0fdfac571b13458
Tesla-Wang <zhuifeng414@126.com>
2021-02-23 09:27:31 +0800

    add offer_30_MinStack & offer_59_maxSlidingWindow

---
+++ 2021/offer_59_maxSlidingWindow.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if len(nums) == 0 or k == 0: return []
+        res = []
+        deque = collections.deque()
+        for i in range(k):
+            while deque and deque[-1] < nums[i]:
+                deque.pop()
+            deque.append(nums[i])
+        res = [deque[0]]
+        for i in range(k, len(nums)):
+            if deque[0] == nums[i-k]:
+                deque.popleft()
+            while deque and deque[-1] < nums[i]:
+                deque.pop()
+            deque.append(nums[i])
+            res.append(deque[0])
+        return res
